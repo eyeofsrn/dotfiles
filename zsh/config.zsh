2f8a78abefd3814ef672aa486415d7edd99026b2
eyeofsrn <sprzy@protonmail.com>
2025-03-27 19:15:59 -0400

    update to working order

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -1,79 +1,44 @@
-# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
-# Initialization code that may require console input (password prompts, [y/n]
-# confirmations, etc.) must go above this block; everything else may go below.
+# Powerlevel10k instant prompt
 if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
   source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
 fi
 
-# Path to your oh-my-zsh installation.
+# Oh-My-Zsh
 export ZSH="/usr/share/oh-my-zsh"
-
-# Uncomment the following line if pasting URLs and other text is messed up.
 DISABLE_MAGIC_FUNCTIONS="true"
-
-# Uncomment the following line to enable command auto-correction.
 ENABLE_CORRECTION="true"
-
-# Uncomment the following line to display red dots whilst waiting for completion.
 COMPLETION_WAITING_DOTS="true"
-
-# Which plugins would you like to load?
-# Standard plugins can be found in $ZSH/plugins/
-# Custom plugins may be added to $ZSH_CUSTOM/plugins/
-# Example format: plugins=(rails git textmate ruby lighthouse)
-# Add wisely, as too many plugins slow down shell startup.
 [[ -z "${plugins[*]}" ]] && plugins=(git fzf extract colored-man-pages)
-
 source $ZSH/oh-my-zsh.sh
 
-# User configuration
-
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-# Ignore commands that start with spaces and duplicates.
-
+# History settings
 export HISTCONTROL=ignoreboth
-
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-# Don't add certain commands to the history file.
-
 export HISTIGNORE="&:[bf]g:c:clear:history:exit:q:pwd:* --help"
-
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-# Make new shells get the history lines from all previous
-# shells instead of the default "last window closed" history.
-
 export PROMPT_COMMAND="history -a; $PROMPT_COMMAND"
 
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-# Set aliases
+# Aliases
 alias hx="sudo -E helix"
-
-# Cleanup orphaned packages
-alias cleanup="sudo pacman -Rsn $(pacman -Qtdq)"
-
-# Get the error messages from journalctl
-alias jctl="journalctl -p 3 -xb"
-
-# Recent installed packages
+alias ls="lsd --icon never"
 alias rip="expac --timefmt='%Y-%m-%d %T' '%l\t%n %v' | sort | tail -200 | nl"
 
+# Theme and plugins
 source /usr/share/zsh-theme-powerlevel10k/powerlevel10k.zsh-theme
-
-# Fish-like syntax highlighting and autosuggestions
 source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 source /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
-
-# Use history substring search
 source /usr/share/zsh/plugins/zsh-history-substring-search/zsh-history-substring-search.zsh
-
-# pkgfile "command not found" handler
 source /usr/share/doc/pkgfile/command-not-found.zsh
 
-# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
+# Load powerlevel10k configuration
 [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
-
 export FZF_BASE=/usr/share/fzf
+
+# Zoxide (smart cd)
+eval "$(zoxide init zsh --cmd cd)"
+
+# Double escape to recall last command
+last_command_recall() {
+  BUFFER=$(fc -ln -1)
+  zle end-of-line
+}
+zle -N last_command_recall
+bindkey '\e\e' last_command_recall
